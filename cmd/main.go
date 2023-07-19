73f16b851776237c08283b701d350d4a17027865
student31415 <qwerty65k@mail.ru>
2023-07-19 21:47:49 +0300

    testing exec cmd

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"os"
+
 	"github.com/GOodCoffeeLover/MasterDiploma/internal/exec"
 )
 
 func main() {
-	executor := exec.NewExecutor()
-	executor.Exec()
+	// executor := exec.NewExecutor()
+	// executor.Exec()
+	err := exec.ExecCmdExample("test", "ls", os.Stdin, os.Stdout, os.Stderr)
+	if err != nil {
+		panic(err)
+	}
 }
--- internal/exec/exec.go
+++ internal/exec/exec.go
@@ -1,6 +1,67 @@
 package exec
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/kubernetes/scheme"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/remotecommand"
+)
+
+// ExecCmd exec command on specific pod and wait the command's output.
+func ExecCmdExample(podName string, command string,
+	stdin io.Reader, stdout io.Writer, stderr io.Writer) error {
+	fmt.Println("Executing...")
+
+	config := &restclient.Config{
+		Host: "http://localhost:8080",
+	}
+	config.GroupVersion = &schema.GroupVersion{Version: "v1"}
+	config.NegotiatedSerializer = runtime.NewSimpleNegotiatedSerializer(runtime.SerializerInfo{})
+
+	client, err := restclient.RESTClientFor(config)
+	if err != nil {
+		return err
+	}
+	cmd := []string{
+		"sh",
+		"-c",
+		command,
+	}
+	req := client.Post().Resource("pods").Name(podName).Namespace("default").SubResource("exec")
+	option := &v1.PodExecOptions{
+		Command: cmd,
+		Stdin:   true,
+		Stdout:  true,
+		Stderr:  true,
+		TTY:     true,
+	}
+	if stdin == nil {
+		option.Stdin = false
+	}
+	req.VersionedParams(
+		option,
+		scheme.ParameterCodec,
+	)
+	exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+	if err != nil {
+		return err
+	}
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdin:  stdin,
+		Stdout: stdout,
+		Stderr: stderr,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 type Executor struct{}
 
