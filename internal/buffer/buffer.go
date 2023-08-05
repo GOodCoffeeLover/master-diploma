db0c6e2bbfae98a3e53b27c97e3e0acd4c1264be
student31415 <qwerty65k@mail.ru>
2023-08-06 00:13:26 +0300

    add tests

---
+++ internal/buffer/buffer.go
@@ -0,0 +1,96 @@
+package buffer
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/GOodCoffeeLover/MasterDiploma/internal/remoteExecuctor"
+	exec "github.com/GOodCoffeeLover/MasterDiploma/internal/remoteExecuctor"
+)
+
+type BufferReadWriteCloser struct {
+	ch chan byte
+}
+
+func NewBufferReadWriteCloser(n uint) BufferReadWriteCloser {
+	ch := make(chan byte, n)
+	return BufferReadWriteCloser{
+		ch: ch,
+	}
+}
+
+func (brw BufferReadWriteCloser) Read(buf []byte) (int, error) {
+	var i int
+	for i = 0; i < len(buf); i++ {
+		b, ok := <-brw.ch
+		if !ok && i == 0 {
+			return 0, io.EOF
+		}
+		if !ok {
+			return i, nil
+		}
+		buf[i] = b
+
+		exec.PrintlnRaw(os.Stderr, fmt.Sprintf("read from chan %v (%v)", b, string(b)))
+	}
+	return i, nil
+}
+
+func (brw BufferReadWriteCloser) Write(buf []byte) (int, error) {
+	for _, b := range buf {
+		brw.ch <- b
+
+		exec.PrintlnRaw(os.Stderr, fmt.Sprintf("write to chan %v (%v)", b, string(b)))
+	}
+	return len(buf), nil
+}
+
+func (brw BufferReadWriteCloser) Close() error {
+	close(brw.ch)
+	exec.PrintlnRaw(os.Stderr, fmt.Sprintf("Close chan"))
+	return nil
+}
+
+func FromReaderToChan(in io.Reader, out chan<- byte) {
+	for {
+		b := make([]byte, 1)
+		n, err := in.Read(b)
+		remoteExecuctor.PrintlnRaw(os.Stderr, fmt.Sprintf("read from in reader %v bytes", n))
+
+		if err == io.EOF {
+			remoteExecuctor.PrintlnRaw(os.Stderr, "closed reader")
+			close(out)
+			return
+		}
+		remoteExecuctor.PrintlnRaw(os.Stderr, fmt.Sprintf("frorm reader %v (%v)", b, string(b)))
+		must(err, "error while reading")
+		for i := 0; i < n; i++ {
+
+			out <- b[i]
+		}
+
+	}
+}
+
+func FromChanToWriter(in <-chan byte, out io.WriteCloser) {
+	for {
+		b, ok := <-in
+		if !ok {
+			remoteExecuctor.PrintlnRaw(os.Stderr, "closed chan")
+			must(out.Close(), "error while closing write")
+			return
+		}
+
+		remoteExecuctor.PrintlnRaw(os.Stderr, fmt.Sprintf("from chan %v (%v)", b, string(b)))
+		n, err := out.Write([]byte{b})
+		remoteExecuctor.PrintlnRaw(os.Stderr, fmt.Sprintf("write to out writer %v bytes", n))
+		must(err, "error while writing")
+	}
+}
+
+func must(err error, msg string) {
+	if err != nil {
+		panic(fmt.Errorf("%v: %w", msg, err))
+	}
+}
---
+++ internal/buffer/buffer_test.go
@@ -0,0 +1,74 @@
+package buffer
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestBasic(t *testing.T) {
+	buf := NewBufferReadWriteCloser(10)
+	input := []byte("string")
+	output := make([]byte, len(input))
+
+	n, err := buf.Write(input)
+	if err != nil {
+		t.Errorf("failed to write: %v", err)
+	}
+	if n != len(input) {
+		t.Errorf("trying to write %v bytes, but write %v", len(input), n)
+	}
+	n, err = buf.Read(output)
+
+	if err != nil {
+		t.Errorf("failed to read: %v", err)
+	}
+	if n != len(output) {
+		t.Errorf("trying to read %v bytes, but read %v", len(output), n)
+	}
+}
+
+func TestFromReaderToChan(t *testing.T) {
+	input := "someString\nstrstrs"
+	reader := strings.NewReader(input)
+	outChan := make(chan byte)
+
+	go FromReaderToChan(reader, outChan)
+
+	output := []byte{}
+	for b := range outChan {
+		output = append(output, b)
+	}
+	if string(output) != input {
+		t.Errorf("input( %v ) not equal to output( %v )", input, string(output))
+	}
+}
+
+type testBuffer struct {
+}
+
+var buf []byte
+
+func (b testBuffer) Write(bytes []byte) (int, error) {
+	buf = append(buf, bytes...)
+	return len(bytes), nil
+}
+
+func (buf testBuffer) Close() error { return nil }
+
+func TestFromChanToWriter(t *testing.T) {
+	input := "someString\nstrstrs"
+	ch := make(chan byte)
+	outputBuf := testBuffer{}
+
+	go func() {
+		for _, b := range []byte(input) {
+			ch <- b
+		}
+		close(ch)
+	}()
+	FromChanToWriter(ch, outputBuf)
+	output := buf
+	if string(output) != input {
+		t.Errorf("input( %v ) not equal to output( %v )", input, string(output))
+	}
+}
